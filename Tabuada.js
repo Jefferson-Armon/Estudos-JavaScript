3c1d3e3e7a9c4a8f6a3fcb9d8b18457eb22ae497
Glauco <jefferson.asrs@gmail.com>
2022-10-13 17:02:19 -0300

    initial commit

---
+++ Tabuada.js
@@ -0,0 +1,30 @@
+/*1) Faça um programa que receba um número e escreva a sua respectiva tabuada.
+    Exemplo:
+        Entrada:
+            2
+
+        Saída:
+            2x1 = 2
+            2x2 = 4
+            2x3 = 6
+            2x4 = 8
+            2x5 = 10
+            2x6 = 12
+            2x7 = 14
+            2x8 = 16
+            2x9 = 18
+            2x10 = 20*/
+
+
+const {gets, print} = require 
+let resultado = 0
+tabuada = function (numeroEscolhido) {
+    for (i=1; i < 11; i++) {
+    resultado = numeroEscolhido * i
+    console.log(`2 x ${i} = ${resultado}`)
+    }
+}
+tabuada(2)
+
+
+    
